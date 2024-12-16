4cd316cfec33a4b1a41fcc25dc9572179beb4d46
thio.perdana <thio.perdana@outlook.com>
2024-12-16 21:53:40 +0700

    tambah foto

--- pages/2_biodata.py
+++ pages/2_biodata.py
@@ -6,10 +6,18 @@ st.header("THIO PERDANA")
 col_1, col_2 = st.columns([1,2])
 
 with col_1:
+    st.image("assets/images/image.jpg")
+
+
     st.subheader("Detail Pribadi")
     st.text("Nama: Thio Perdana")
     st.text("Tanggal Lahir: 23 April 1998")
     st.text("Jenis Kelamin: Laki-Laki")
+
+    st.subheader("Hobby")
+    st.text("Olahraga: Sepak Bola")
+    st.text("Makanan Favorit: Nasi Goreng")
+    st.text("Minuman Favorit: Kopi")
 
 with col_2:
     st.subheader("Alamat")
@@ -17,3 +25,14 @@ with col_2:
     st.text("Kecamatan Bulaksumur")
     st.text("Kabupaten Bekasi")
     st.text("Provinsi Jawa Barat")
+
+    st.subheader("Kegiatan Sehari-Hari")
+    st.text("Mengikuti workshop dan kursus online")
+    st.text("Mencari informasi lowongan pekerjaan")
+    st.text("Merencanakan perjalanan")
+
+    st.subheader("Informasi Kontak")
+    st.text("Email: thio.perdana@gmail.com")
+    st.text("No. Telpon: 081234567890")
+    st.text("Facebook: thioperdana")
+    st.text("Instagram: thioperdana")
